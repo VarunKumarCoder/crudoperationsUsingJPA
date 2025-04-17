cba842b9e24a0a123443ff3797694fe69cae2bee
VarunKumarCoder <medavarunkumar@gmail.com>
2025-04-17 22:17:50 +0530

    full object modification

---
+++ 1008_FullObjectModification/src/main/java/com/cd/runner/CrudRepoTestRunner.java
@@ -0,0 +1,31 @@
+package com.cd.runner;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import com.cd.entity.Doctor;
+import com.cd.service.IDoctorService;
+
+@Component
+public class CrudRepoTestRunner implements CommandLineRunner {
+	
+	@Autowired
+	private IDoctorService docService;
+
+	@Override
+	public void run(String... args) throws Exception {
+		Doctor doc=new Doctor();
+		doc.setId(2);
+		doc.setDocName("Reh");
+		doc.setIncome(14000.00);
+		doc.setPlace("MBI");
+		doc.setSpecialization("Trauma");
+		System.out.println(docService.registerOrUpdateDoctor(doc));
+		
+	}
+
+	
+	
+
+}
