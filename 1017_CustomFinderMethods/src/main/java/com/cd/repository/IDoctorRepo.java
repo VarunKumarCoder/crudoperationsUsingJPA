2438ec247957a75b645cd16c919245cd0ddd3175
VarunKumarCoder <medavarunkumar@gmail.com>
2025-04-18 17:11:39 +0530

    custom finder methods

--- 1017_CustomFinderMethods/src/main/java/com/cd/repository/IDoctorRepo.java
+++ 1017_CustomFinderMethods/src/main/java/com/cd/repository/IDoctorRepo.java
@@ -1,7 +1,8 @@
 package com.cd.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import com.cd.entity.Doctor;
@@ -9,4 +10,8 @@ import com.cd.entity.Doctor;
 @Repository
 public interface IDoctorRepo extends JpaRepository<Doctor, Integer> {
 
+	//public List<Doctor> findByDocNameEquals(String name);
+	//public List<Doctor> findBydocNameStartingWith(char chares);
+	//public Iterable<Doctor> findBydocNameEndingWith(char chars);
+	public Iterable<Doctor> findBydocNameContaining(String name);
 }
--- 1017_CustomFinderMethods/src/main/java/com/cd/runner/CrudRepoTestRunner.java
+++ 1017_CustomFinderMethods/src/main/java/com/cd/runner/CrudRepoTestRunner.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import com.cd.repository.IDoctorRepo;
 import com.cd.service.IDoctorService;
 
 @Component
@@ -11,11 +12,17 @@ public class CrudRepoTestRunner implements CommandLineRunner {
 	
 	@Autowired
 	private IDoctorService docService;
+	
+	@Autowired
+	private IDoctorRepo docRepo;
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println(docService.fetchById(9));
 		
+		//docRepo.findByDocNameEquals("chetan").forEach(System.out::println);
+		//docRepo.findBydocNameStartingWith('s').forEach(System.out::println);
+		//docRepo.findBydocNameEndingWith('r').forEach(System.out::println);
+		docRepo.findBydocNameContaining("ai").forEach(System.out::println);
 		
 	}
 
