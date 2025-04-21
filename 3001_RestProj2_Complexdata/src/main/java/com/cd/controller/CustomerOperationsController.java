71c34e0b7c9033343270ce697708770d891ffcff
VarunKumarCoder <medavarunkumar@gmail.com>
2025-04-21 17:27:30 +0530

    group data added

--- 3001_RestProj2_Complexdata/src/main/java/com/cd/controller/CustomerOperationsController.java
+++ 3001_RestProj2_Complexdata/src/main/java/com/cd/controller/CustomerOperationsController.java
@@ -24,4 +24,14 @@ public class CustomerOperationsController {
 		HttpStatus status=HttpStatus.OK;
 		return new ResponseEntity<Customer>(cust,status);
 	}
+	
+	@GetMapping("/Groupreport")
+	public ResponseEntity<List<Customer>> showGroupData(){
+		Customer cust=new Customer(1001,"raja",6652.2f,new String[] {"Red","blue","green"},List.of("10th","Inter","B.tech"),Set.of(66666666L,2323232L,4545454L),Map.of("aadhar",454545L,"panNo",78787L), new Company("Samsung","HYD","Electronics",4));
+		Customer cust1=new Customer(1002,"Faisal",6652.2f,new String[] {"Red","blue","green"},List.of("10th","Inter","B.tech"),Set.of(66666666L,2323232L,4545454L),Map.of("aadhar",454545L,"panNo",78787L), new Company("Samsung","HYD","Electronics",4));
+		Customer cust2=new Customer(1003,"varun",6652.2f,new String[] {"Red","blue","green"},List.of("10th","Inter","B.tech"),Set.of(66666666L,2323232L,4545454L),Map.of("aadhar",454545L,"panNo",78787L), new Company("Samsung","HYD","Electronics",4));
+		
+		HttpStatus status=HttpStatus.OK;
+		return new ResponseEntity<List<Customer>>(List.of(cust,cust1,cust2),status);
+	}
 }
