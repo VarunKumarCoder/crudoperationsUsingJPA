fe81f2188554eef800649961ad9c5352893b9449
VarunKumarCoder <medavarunkumar@gmail.com>
2025-04-21 17:21:54 +0530

    final commit

--- 3001_RestProj2_Complexdata/src/main/java/com/cd/controller/CustomerOperationsController.java
+++ 3001_RestProj2_Complexdata/src/main/java/com/cd/controller/CustomerOperationsController.java
@@ -6,6 +6,7 @@ import java.util.Set;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +17,7 @@ import com.cd.model.Customer;
 @RequestMapping("/complexdata")
 public class CustomerOperationsController {
 
+	@GetMapping("/report")
 	public ResponseEntity<Customer> showComplexData(){
 		Customer cust=new Customer(1001,"raja",6652.2f,new String[] {"Red","blue","green"},List.of("10th","Inter","B.tech"),Set.of(66666666L,2323232L,4545454L),Map.of("aadhar",454545L,"panNo",78787L), new Company("Samsung","HYD","Electronics",4));
 	
